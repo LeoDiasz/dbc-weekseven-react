9d36b90aa1f347ae34add829638c18c310bfed09
Leonardo Diasz <leonardoduarte.multimidia@gmail.com>
2022-07-21 10:59:08 -0300

    arrumando bug do node modules

--- aula02-terca-1907/homework/marioproject/src/App.js
+++ aula02-terca-1907/homework/marioproject/src/App.js
@@ -1,7 +1,5 @@
 import "./app.css"
-import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
-import Home from "./pages/Home/Home";
+
 
 export function App() {
   return (
